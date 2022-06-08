df8058a9d3d68a18a0dced5f308a92f8595c0a1b
Max De George <maxtdegeorge@gmail.com>
2022-06-07 21:29:11 -0400

    initial build

---
+++ BudgetingApp/BudgetingApp/BudgetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetingAppApp.swift
+//  BudgetingApp
+//
+//  Created by Max De George on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BudgetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
